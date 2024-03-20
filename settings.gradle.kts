72548d6297effab97afb7a656b585842efb25477
ringring <ringring.creator@gmail.com>
2024-03-20 09:57:47 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.ring.ring.arch-basic-kt"
---
+++ src/main/kotlin/com/ring/ring/Application.kt
@@ -0,0 +1,15 @@
+package com.ring.ring
+
+import com.ring.ring.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureRouting()
+}
