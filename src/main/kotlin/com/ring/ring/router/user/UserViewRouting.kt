ef023ef4e4f7bc6afd4ecd2f0bb13629cb3b7eff
ringring <ringring.creator@gmail.com>
2024-03-22 11:34:28 +0900

    Added redirect to login screen

--- src/main/kotlin/com/ring/ring/router/user/UserViewRouting.kt
+++ src/main/kotlin/com/ring/ring/router/user/UserViewRouting.kt
@@ -5,10 +5,16 @@ import com.ring.ring.ui.user.signUpView
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.html.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 
 fun Route.userViewRouting() {
+    route("") {
+        get {
+            call.respondRedirect("/user/login")
+        }
+    }
     route("/user") {
         get("signup") {
             call.respondHtml(HttpStatusCode.OK) { signUpView() }
